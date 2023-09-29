811bda2cb5ff9df9180628e34dd4147369885c62
James <adolfssonjames@gmail.com>
2023-09-29 17:31:36 +0200

    dynamically rendering staff

--- canvas-app/src/components/Contact/ContactStaff.tsx
+++ canvas-app/src/components/Contact/ContactStaff.tsx
@@ -1,41 +1,49 @@
+const staffMembers = [
+  {
+    name: "Ulrika",
+    role: "Grundare & säljare",
+    email: "Ulrika@Mail.com",
+    phone: "+46 73 111 11 11",
+    imageSrc: "images/testname2.jpg",
+  },
+  {
+    name: "Carina",
+    role: "Grundare & säljare",
+    email: "Carina@Mail.com",
+    phone: "+46 73 222 22 22",
+    imageSrc: "images/testname1.jpg",
+  },
+];
+
+// Create a StaffMember component
+function StaffMember({ name, role, email, phone, imageSrc }: any) {
+  return (
+    <div className="h-96 pl-5 pr-5 flex items-center justify-center flex-col rounded-xl gap-2">
+      <img
+        className="relative h-auto w-52 rounded-full drop-shadow-md mb-5"
+        src={imageSrc}
+        alt={`Profile-picture of ${name}`}
+      />
+      <div className="flex flex-col relative bg-white/70 rounded-xl p-4">
+        <h2 className="text-3xl relative">{name}</h2>
+        <p className="text-lg font-medium">{role}</p>
+        <a className="relative" href={`mailto:${email}`}>
+          {email}
+        </a>
+        <a className="relative" href={`tel:${phone}`}>
+          {phone}
+        </a>
+      </div>
+    </div>
+  );
+}
+
 export default function ContactStaff() {
   return (
-    <section className=" flex flex-row items-center justify-center gap-20 pt-16 pb-10 flex-wrap ">
-      <div className=" h-96 pl-5 pr-5 flex items-center justify-center flex-col rounded-xl gap-2 ">
-        <img
-          className="relative h-auto w-52 rounded-full drop-shadow-md mb-5"
-          src="images/testname2.jpg"
-          alt="Profile-picture"
-        />
-        <div className="flex flex-col relative bg-white/70 rounded-xl p-4  ">
-          <h2 className="text-3xl relative">Ulrika</h2>
-          <p className="text-lg font-medium"> Grundare & säljare </p>
-          <a className=" relative" href="mailto: Ulrika@Mail.com">
-            Ulrika@Mail.com
-          </a>
-          <a className=" relative" href="tel:0123456789">
-            +46 73 111 11 11
-          </a>
-        </div>
-      </div>
-
-      <div className="h-96 pl-5 pr-5 flex items-center justify-center flex-col rounded-xl gap-2">
-        <img
-          className="relative h-auto w-52 rounded-full drop-shadow-md mb-5"
-          src="images/testname1.jpg"
-          alt="Profile-picture"
-        />
-        <div className="flex flex-col relative bg-white/70 rounded-xl p-4">
-          <h2 className="text-3xl relative">Carina</h2>
-          <p className="text-lg font-medium"> Grundare & säljare </p>
-          <a className=" relative" href="mailto: Ulrika@Mail.com">
-            Ulrika@Mail.com
-          </a>
-          <a className=" relative" href="tel:0123456789">
-            +46 73 111 11 11
-          </a>
-        </div>
-      </div>
+    <section className="flex flex-row items-center justify-center gap-20 pt-16 pb-10 flex-wrap">
+      {staffMembers.map((staff, index) => (
+        <StaffMember key={index} {...staff} />
+      ))}
     </section>
   );
 }
